b064b63127f53e543d0b74e998c392190331adf1
Fahad Malik <unexpectedprogrammer@gmail.com>
2023-04-01 16:23:14 +0500

    changes made to the backend of app

--- rewards/urls.py
+++ rewards/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("spin-wheel", views.SpinWheelView.as_view()),
-    path('daily-check-in', views.DailyCheckIn.as_view())
+    path('daily-check-in', views.DailyCheckIn.as_view()),
+    path("wallet", views.WalletView.as_view(), name="wallet-view")
 ]
--- rewards/views.py
+++ rewards/views.py
@@ -18,20 +18,20 @@ class SpinWheelView(APIView):
         # Get the number of points earned from the spin wheel from the POST request data
         points = request.data.get('points')
         if not points:
-            return Response({'error': 'Points parameter is missing'}, status=400)
+            return Response({'message': 'Points parameter is missing'}, status=400)
 
         # Get the authenticated user from the request
         user = request.user
 
-        RecentEarnings.objects.create(user=user, way_to_earn="Spin Wheel", point_earned=points)
-        RecentEarnings.save()
+        user_recent_earning = RecentEarnings.objects.create(user=user, way_to_earn="Spin Wheel", point_earned=points)
+        user_recent_earning.save()
 
         # Add the points to the user's account
         user_wallet = Wallet.objects.get(user=user)
         user_wallet.points += points
         user_wallet.save()
 
-        return Response({'success': f'{points} points added to your account.'}, status=200)
+        return Response({'message': f'{points} points added to your account.'}, status=200)
 
 
 class DailyCheckIn(APIView):
@@ -45,7 +45,7 @@ class DailyCheckIn(APIView):
         # Check if the user has already earned points for a daily check-in in the last 24 hours
         last_check_in = RecentEarnings.objects.filter(user=user, way_to_earn='Daily Check-In').order_by('-created_at').first()
         if last_check_in and timezone.now() - last_check_in.created_at < timedelta(days=1):
-            return Response({'error': 'You have already earned points for a daily check-in in the last 24 hours.'}, status=400)
+            return Response({'message': 'You have already earned points for a daily check-in in the last 24 hours.'}, status=400)
 
         # Add the points to the user's account
         user_wallet = Wallet.objects.get(user=user)
@@ -54,4 +54,21 @@ class DailyCheckIn(APIView):
 
         RecentEarnings.objects.create(user=user, way_to_earn="Daily Check-In", point_earned=50)
 
-        return Response({'success': f'{50} points added to your account.'}, status=200)
+        return Response({'message': f'{50} points added to your account.'}, status=200)
+    
+
+class WalletView(APIView):
+    authentication_classes = [TokenAuthentication]
+
+    def get(self, request, *args, **kwargs):
+        # Get the authenticated user from the request
+        user = request.user
+
+        # Get the user's wallet
+        try:
+            user_wallet = Wallet.objects.select_related('user').get(user=user)
+        except Wallet.DoesNotExist:
+            return Response({'message': 'Wallet does not exist'}, status=400)
+
+        # Return the current points in the user's wallet
+        return Response({'message': str(user_wallet.points)}, status=200)
