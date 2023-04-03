df4dd623945b3777ae997ea337baff9da98e3991
Fahad Malik <69435725+fahad-programmer@users.noreply.github.com>
2023-04-03 21:48:23 +0500

    solved a bug in the urls.py

--- Profile/urls.py
+++ Profile/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('referral', views.ReferralView.as_view()),
     path('resend-verification-email', views.ResendVerificationEmail.as_view(), name='resend-verification-email'),
     path('check-user-active/<str:token>', views.CheckUserActive.as_view(), name="check-user-active"),
-    path('get-refferals', views.ReferralList.as_view(), name="refferal users")
+    path('get-refferals', views.ReferralList.as_view(), name="refferal users"),
     path('get-user-code', views.UserCodeAPIView.as_view(), name="userCode")
 
    ]
