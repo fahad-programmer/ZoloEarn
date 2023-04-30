8241fbd23991e91bf6004e2382bafacc60a71774
Fahad Malik <unexpectedprogrammer@gmail.com>
2023-05-01 00:30:06 +0500

    changes made to the api and added ttc functions

--- rewards/urls.py
+++ rewards/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('daily-check-in', views.DailyCheckIn.as_view()),
     path("wallet", views.WalletView.as_view(), name="wallet-view"),
     path('get-user-spin-turn', views.UserSpinTurn.as_view()),
-    path('add-user-spin-turn', views.UserSpinFree.as_view())
+    path('add-user-spin-turn', views.UserSpinFree.as_view()),
+    path("get-user-ttc-turns", views.userTTCAvailabeTurn.as_view()),
+    path("add-ttc-turn", views.addUserTTCTurn.as_view()),
+    path('ttc-turn', views.TTCApiView.as_view())
 ]
