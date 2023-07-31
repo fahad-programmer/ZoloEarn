3feb43c425685916a6eba88bb02d64f33b9aa481
Fahad Malik <69435725+fahad-programmer@users.noreply.github.com>
2023-07-31 22:36:50 +0500

    changed the app version to `2.3.6`

--- Profile/views.py
+++ Profile/views.py
@@ -709,7 +709,7 @@ class HelpCenterAPIView(APIView):
 
 class VersionCheck(APIView):
     def get(self, request, *args, **kwargs):
-        latest_version = "2.3.5"
+        latest_version = "2.3.6"
         return Response({"message": latest_version})
 
 
